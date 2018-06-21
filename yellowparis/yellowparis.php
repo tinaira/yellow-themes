1deeb9d15a7238790f152bc93278e8710e83ed5b
Arianit Kukaj <mr.hedho@yahoo.com>
2018-06-21 03:37:46 +0200

    Add files via upload

---
+++ yellowparis/yellowparis.php
@@ -0,0 +1,12 @@
+<?php
+// Yellow Paris theme, https://github.com/tinaira/YellowParis
+// Copyright (c) 2013-2018 Datenstrom, https://datenstrom.se
+// This file may be used and distributed under the terms of the public license.
+
+class YellowThemeYellowParis
+{
+	const VERSION = "0.7.4";	
+}
+
+$yellow->themes->register("yellowparis", "YellowThemeParis", YellowThemeYellowParis::VERSION);
+?>
